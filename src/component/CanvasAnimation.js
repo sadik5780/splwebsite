fe31475bc788fe427786e2d43f3a7859ac28e3df
sadik <sadik.s@cloudxperte.com>
2024-12-21 18:49:38 +0530

    Initial commit

---
+++ src/component/CanvasAnimation.js
@@ -0,0 +1,81 @@
+import React, { useRef, useEffect } from 'react';
+
+const CanvasAnimation = () => {
+  const canvasRef = useRef(null);
+
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    const context = canvas.getContext('2d');
+    let particles = [];
+    const particleRadius = 2.5;
+
+    const random = (max) => Math.random() * max;
+
+    const resizeCanvas = () => {
+      const width = window.innerWidth;
+      const height = Math.max(
+        document.body.scrollHeight,
+        document.body.offsetHeight,
+        document.documentElement.clientHeight,
+        document.documentElement.scrollHeight,
+        document.documentElement.offsetHeight
+      );
+
+      canvas.width = width;
+      canvas.height = height;
+
+      const numParticles = Math.random() * 100 + width / 5;
+      particles = Array.from({ length: numParticles }, () => ({
+        color: `rgb(${random(255)},${random(255)},${random(255)})`,
+        x: random(width),
+        y: random(height),
+        velocity: random(3) + 1,
+      }));
+    };
+
+    const drawParticles = () => {
+      context.clearRect(0, 0, canvas.width, canvas.height);
+      particles.forEach((particle) => {
+        context.shadowBlur = 10;
+        context.shadowColor = particle.color;
+        context.fillStyle = particle.color;
+        context.beginPath();
+        context.arc(particle.x, particle.y, particleRadius, 0, Math.PI * 2);
+        context.fill();
+        particle.y -= particle.velocity;
+        if (particle.y < -particleRadius) {
+          particle.y = canvas.height;
+        }
+      });
+    };
+
+    const animate = () => {
+      drawParticles();
+      requestAnimationFrame(animate);
+    };
+
+    window.addEventListener('resize', resizeCanvas);
+    resizeCanvas();
+    animate();
+
+    return () => {
+      window.removeEventListener('resize', resizeCanvas);
+    };
+  }, []);
+
+  return <canvas
+  id="c"
+  ref={canvasRef}
+  style={{
+    position: 'fixed',
+    top: 0,
+    left: 0,
+    width: '100%',
+    height: '100%',
+    zIndex: 1, // Adjust as needed; make it higher to overlay content
+    pointerEvents: 'none', // Prevent interaction blocking
+  }}
+/>;
+};
+
+export default CanvasAnimation;
---
+++ src/component/slider.js
@@ -0,0 +1,203 @@
+import React, { useState } from "react";
+import { motion, AnimatePresence } from "framer-motion";
+import Canvasanimation from "./CanvasAnimation";
+import playersData from "./playersData.json"; 
+
+const Slider = () => {
+  const [players, setPlayers] = useState(playersData);
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  // Create an Audio instance for the main.mp3 sound
+  const audio = new Audio("/images/main.mp3"); 
+
+  const handleSold = (id) => {
+    setPlayers((prevPlayers) =>
+      prevPlayers.map((player) =>
+        player.id === id
+          ? { ...player, sold: true, gifPlayed: true }
+          : player
+      )
+    );
+
+    // Play the audio when "MARK SOLD" is clicked
+    audio.play();
+
+    setTimeout(() => {
+      setPlayers((prevPlayers) =>
+        prevPlayers.map((player) =>
+          player.id === id ? { ...player, gifPlayed: false } : player
+        )
+      );
+    }, 2000);
+  };
+
+  const handleNotSold = (id) => {
+    setPlayers((prevPlayers) =>
+      prevPlayers.map((player) =>
+        player.id === id ? { ...player, sold: false, gifPlayed: false } : player
+      )
+    );
+  };
+
+  const nextSlide = () => {
+    setCurrentIndex((prevIndex) =>
+      prevIndex === players.length - 1 ? 0 : prevIndex + 1
+    );
+  };
+
+  const prevSlide = () => {
+    setCurrentIndex((prevIndex) =>
+      prevIndex === 0 ? players.length - 1 : prevIndex - 1
+    );
+  };
+
+  const currentPlayer = players[currentIndex];
+
+  // const imageVariants = {
+  //   initial: { opacity: 0, x: -100 },
+  //   animate: { opacity: 1, x: 0, transition: { duration: 1 } },
+  //   exit: { opacity: 0, x: -100, transition: { duration: 1 } },
+  // };
+
+  // const textVariants = {
+  //   initial: { opacity: 0, x: 100 },
+  //   animate: { opacity: 1, x: 0, transition: { duration: 1 } },
+  //   exit: { opacity: 0, x: 100, transition: { duration: 1 } },
+  // };
+
+  return (
+    <div
+      className="slider-container"
+      style={{ backgroundImage: `url("/images/bg.png")` }}
+    >
+      <Canvasanimation />
+      {/* Constant Header */}
+      <div className="header-section">
+        <img src="/images/SPLlogo.png" alt="SPL Logo" className="spl-logo" />
+        <h1>
+          <span style={{ fontWeight: "bold", color: "yellow", margin:'0 10px 0' }}>IMRAN KHANSAHAB</span>{" "}
+          PRESENTS &nbsp; <span className="spl">SPL SEASON-8</span>
+        </h1>
+      </div>
+
+      <div className="constant-text">
+        <span>Presented by</span> <br /> Sadik Shaikh
+      </div>
+
+      <AnimatePresence mode="wait">
+        <motion.div
+          key={currentPlayer.id}
+          className="slider-content"
+          initial="initial"
+          animate="animate"
+          exit="exit"
+        >
+          {currentPlayer.type === "label" ? (
+            <motion.h1
+              key={currentPlayer.id}
+              className="category-label"
+              // variants={textVariants}
+            >
+              {currentPlayer.label}
+            </motion.h1>
+          ) : (
+            <>
+              {/* Player Image */}
+              <AnimatePresence mode="wait">
+                <motion.div
+                  className="player-image"
+                  key={`${currentPlayer.id}-image`}
+                  // variants={imageVariants}
+                >
+                  <div className="player-image-wrapper">
+                    <img
+                      src={currentPlayer.image}
+                      alt={currentPlayer.name}
+                      className={currentPlayer.sold ? "sold-overlay" : ""}
+                    />
+                    {currentPlayer.sold && currentPlayer.gifPlayed && (
+                      <img
+                        src="/images/congratulations-7600.gif"
+                        alt="Congratulations"
+                        className="gif-overlay"
+                      />
+                    )}
+                    {currentPlayer.sold && !currentPlayer.gifPlayed && (
+                      <img
+                        src="/images/Stamp-01.png"
+                        alt="Stamp"
+                        className="stamp-overlay"
+                      />
+                    )}
+                    {currentPlayer.sold && currentPlayer.gifPlayed && (
+                      <img
+                        src="/images/Stamp.gif"
+                        alt="Stamp Animation"
+                        className="gif-overlay"
+                      />
+                    )}
+                  </div>
+                </motion.div>
+              </AnimatePresence>
+
+              {/* Player Info */}
+              <AnimatePresence mode="wait">
+                <motion.div
+                  className="player-info"
+                  key={`${currentPlayer.id}-info`}
+                  // variants={textVariants}
+                >
+                  <h2>
+                    Name : <br /> <span> {currentPlayer.name} </span>
+                  </h2>
+                  <div className="desc_text">
+                    <div className="splty">
+                      <h3>
+                        Speciality <br />{" "}
+                        <span> {currentPlayer.speciality} </span>
+                      </h3>
+                    </div>
+                    <div className="age">
+                      <h3>
+                        Age <br /> <span> {currentPlayer.age}</span>
+                      </h3>
+                    </div>
+                  </div>
+                  <div className="button-group">
+                    <button
+                      onClick={() => handleSold(currentPlayer.id)}
+                      disabled={currentPlayer.sold}
+                      style={{
+                        backgroundColor: currentPlayer.sold ? "gray" : "green",
+                      }}
+                    >
+                      {currentPlayer.sold ? "SOLD" : "MARK SOLD"}
+                    </button>
+                    <button
+                      onClick={() => handleNotSold(currentPlayer.id)}
+                      style={{
+                        backgroundColor: currentPlayer.sold ? "red" : "gray",
+                      }}
+                      disabled={!currentPlayer.sold}
+                    >
+                      NOT SOLD
+                    </button>
+                  </div>
+                </motion.div>
+              </AnimatePresence>
+            </>
+          )}
+        </motion.div>
+      </AnimatePresence>
+
+      <div className="navigation">
+        <button onClick={prevSlide} disabled={currentIndex === 0}>
+          Previous
+        </button>
+        <button onClick={nextSlide}>Next</button>
+      </div>
+    </div>
+  );
+};
+
+export default Slider;
